7fe489ca3cc4dbf15fbe37fd134780f87f3f86a2
GentleAndy <andystemidwebguru@gmail.com>
2023-04-16 13:28:14 -0700

    deployed the homepage

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import "./App.css";
+import Home from "../src/Pages/Home/Home";
+
+function App() {
+  return (
+    <div className="app">
+      <Home />
+    </div>
+  );
+}
+
+export default App;
