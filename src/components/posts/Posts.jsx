17c41ab1d1c16af43d74540455f4338489dc6d3c
GentleAndy <andystemidwebguru@gmail.com>
2023-04-30 00:09:29 +0100

    added post from post props

--- src/components/posts/Posts.jsx
+++ src/components/posts/Posts.jsx
@@ -1,18 +1,35 @@
-import React from "react";
+import React, { useState } from "react";
 import "../posts/posts.css";
 import PersonImg from "../../Assets/person.jpg";
 import PostImage from "../../Assets/postImage.jpeg";
 import { MoreVert, ThumbUp, Comment, Share } from "@mui/icons-material";
+import { Users } from "../../dataServices";
 
-const Posts = () => {
+const Posts = ({ post }) => {
+  const [like, setLike] = useState(post.like);
+  const [isliked, setIsliked] = useState(false);
+
+  const likeHandler = () => {
+    setLike(isliked ? like - 1 : like + 1);
+    setIsliked(!isliked);
+  };
   return (
     <div className="post">
       <div className="postWrapper">
         <div className="postAbout">
           <div className="postAboutLeft">
-            <img className="postAboutLeftImg" src={PersonImg} alt="" />
-            <span className="postAboutLeftName">Temitope Omosebi</span>
-            <span className="postAboutLeftTime">5 mings ago</span>
+            <img
+              className="postAboutLeftImg"
+              src={
+                Users.filter((user) => user.id === post.userId)[0]
+                  .profilePicture
+              }
+              alt=""
+            />
+            <span className="postAboutLeftName">
+              {Users.filter((user) => user.id === post.userId)[0].username}
+            </span>
+            <span className="postAboutLeftTime">{post.date}</span>
           </div>
 
           <div className="postAboutRight">
@@ -21,19 +38,15 @@ const Posts = () => {
         </div>
 
         <div className="postContent">
-          <span className="postArticle">
-            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Possimus
-            officiis quisquam eligendi corrupti? Deleniti, pariatur! Quae
-            pariatur beatae voluptates doloribus modi consectetur nobis?
-          </span>
-          <img className="postMainImg" src={PostImage} alt="" />
+          <span className="postArticle">{post.desc}</span>
+          <img className="postMainImg" src={post.photo} alt="" />
         </div>
 
         <div className="postActivity">
           <div className="postothers">
             <div className="postLikeOthers">
               <ThumbUp className="postLikeOtherscon" />
-              <span>Clyslayde Santos and 3 others</span>
+              <span>Clyslayde Santos and {like} others</span>
             </div>
 
             <div className="postCommentsOthers">
@@ -44,7 +57,7 @@ const Posts = () => {
           <hr className="postHr" />
           <div className="postActivityWarapper">
             <div className="postLike">
-              <ThumbUp className="postActivityIcon" />
+              <ThumbUp onClick={likeHandler} className="postActivityIcon" />
               <span>Like</span>
             </div>
 
