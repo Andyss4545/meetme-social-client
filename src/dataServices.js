f3fcaafdff28790874ff39373c9df3a5df6c6dfe
GentleAndy <andystemidwebguru@gmail.com>
2023-04-30 00:02:41 +0100

    added person assets

---
+++ src/dataServices.js
@@ -0,0 +1,162 @@
+import personImg1 from "../src/Assets/person/1.jpeg"
+import personImg2 from "../src/Assets/person/2.jpeg"
+import personImg3 from "../src/Assets/person/3.jpeg"
+import personImg4 from "../src/Assets/person/4.jpeg"
+import personImg5 from "../src/Assets/person/5.jpeg"
+import personImg6 from "../src/Assets/person/6.jpeg"
+import personImg7 from "../src/Assets/person/7.jpeg"
+import personImg8 from "../src/Assets/person/8.jpeg"
+import personImg9 from "../src/Assets/person/9.jpeg"
+import personImg10 from "../src/Assets/person/10.jpeg"
+
+
+import postImg1 from "../src/Assets/post/1.jpeg"
+import postImg2 from "../src/Assets/post/2.jpeg"
+import postImg3 from "../src/Assets/post/3.jpeg"
+import postImg4 from "../src/Assets/post/4.jpeg"
+import postImg5 from "../src/Assets/post/5.jpeg"
+import postImg6 from "../src/Assets/post/6.jpeg"
+import postImg7 from "../src/Assets/post/7.jpeg"
+import postImg8 from "../src/Assets/post/8.jpeg"
+import postImg9 from "../src/Assets/post/9.jpeg"
+import postImg10 from "../src/Assets/post/10.jpeg"
+
+export const Users = [
+    {
+      id: 1,
+      profilePicture: personImg1,
+      username: "Temitope Omosebi",
+    },
+    {
+      id: 2,
+      profilePicture: personImg2,
+      username: "Jacob Akindele",
+    },
+    {
+      id: 3,
+      profilePicture: personImg3,
+      username: "Ayo Balogun",
+    },
+    {
+      id: 4,
+      profilePicture: personImg4,
+      username: "Dora Akunyili",
+    },
+    {
+      id: 5,
+      profilePicture: personImg5,
+      username: "Peter San",
+    },
+    {
+      id: 6,
+      profilePicture: personImg6,
+      username: "Barrack Obama",
+    },
+    {
+      id: 7,
+      profilePicture: personImg7,
+      username: "Donald Trump",
+    },
+    {
+      id: 8,
+      profilePicture: personImg8,
+      username: "Mathew Thomas",
+    },
+    {
+      id: 9,
+      profilePicture: personImg9,
+      username: "Bubbery Sam",
+    },
+    {
+      id: 10,
+      profilePicture: personImg10,
+      username: "Ayo Oluwaseun",
+    },
+  ];
+  
+  export const posts = [
+    {
+      id: 1,
+      desc: "Love For All, Hatred For None.",
+      photo: postImg1,
+      date: "5 mins ago",
+      userId: 1,
+      like: 32,
+      comment: 9,
+    },
+    {
+      id: 2,
+      photo: postImg2,
+      date: "15 mins ago",
+      userId: 2,
+      like: 2,
+      comment: 1,
+    },
+    {
+      id: 3,
+      desc: "Every moment is a fresh beginning.",
+      photo: postImg3,
+      date: "1 hour ago",
+      userId: 3,
+      like: 61,
+      comment: 2,
+    },
+    {
+      id: 4,
+      photo: postImg4,
+      date: "4 hours ago",
+      userId: 4,
+      like: 7,
+      comment: 3,
+    },
+    {
+      id: 5,
+      photo:  postImg5,
+      date: "5 hours ago",
+      userId: 5,
+      like: 23,
+      comment: 5,
+    },
+    {
+      id: 6,
+      photo: postImg6,
+      date: "1 day ago",
+      userId: 6,
+      like: 44,
+      comment: 6,
+    },
+    {
+      id: 7,
+      desc: "Never regret anything that made you smile.",
+      photo: postImg7,
+      date: "2 days ago",
+      userId: 7,
+      like: 52,
+      comment: 3,
+    },
+    {
+      id: 8,
+      photo: postImg8,
+      date: "3 days ago",
+      userId: 8,
+      like: 15,
+      comment: 1,
+    },
+    {
+      id: 9,
+      desc: "Change the world by being yourself.",
+      photo: postImg9,
+      date: "5 days ago",
+      userId: 9,
+      like: 11,
+      comment: 2,
+    },
+    {
+      id: 10,
+      photo: postImg10,
+      date: "1 week ago",
+      userId: 10,
+      like: 104,
+      comment: 12,
+    },
+  ];
