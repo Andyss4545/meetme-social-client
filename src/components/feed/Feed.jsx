5af1026b50db63678a1b84cf0d535685773cf697
GentleAndy <andystemidwebguru@gmail.com>
2023-05-31 14:11:37 +0100

    get feed data from the server

--- src/components/feed/Feed.jsx
+++ src/components/feed/Feed.jsx
@@ -1,15 +1,33 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import "../feed/feed.css";
+import axios from "axios";
 import CreatePost from "../createPost/CreatePost";
 import Posts from "../posts/Posts";
-import { posts } from "../../dataServices";
+import getPosts from "../../services/postService";
 
 const Feed = () => {
+  const [posts, setPosts] = useState([]);
+
+  useEffect(() => {
+    const fetchPosts = async () => {
+      try {
+        const response = await axios.get(
+          "http://localhost:5500/api/posts/timeline/6429a0049a3debe88e9bf842"
+        );
+        console.log("post", response.data);
+        setPosts(response.data);
+      } catch (error) {
+        setPosts(error);
+      }
+    };
+
+    fetchPosts();
+  }, []);
   return (
     <div className="feed">
       <CreatePost />
 
-      {posts.map((post, index) => {
+      {posts?.map((post, index) => {
         return <Posts key={index} post={post} />;
       })}
     </div>
