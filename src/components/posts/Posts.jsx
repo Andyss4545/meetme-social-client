47d4435d25b87491f806c7db8b648dd5e897230c
GentleAndy <andystemidwebguru@gmail.com>
2023-05-31 14:13:03 +0100

    get user data from server

--- src/components/posts/Posts.jsx
+++ src/components/posts/Posts.jsx
@@ -1,35 +1,50 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import "../posts/posts.css";
 import PersonImg from "../../Assets/person.jpg";
 import PostImage from "../../Assets/postImage.jpeg";
 import { MoreVert, ThumbUp, Comment, Share } from "@mui/icons-material";
-import { Users } from "../../dataServices";
+import getUser from "../../services/userService";
+import profileAvatar from "../../Assets/avatar.jpg";
+import { format } from "timeago.js";
+import { Link } from "react-router-dom";
 
 const Posts = ({ post }) => {
-  const [like, setLike] = useState(post.like);
+  const [like, setLike] = useState(post.likes.length);
   const [isliked, setIsliked] = useState(false);
+  const [user, setUser] = useState([]);
+  console.log("posts", post);
 
   const likeHandler = () => {
     setLike(isliked ? like - 1 : like + 1);
     setIsliked(!isliked);
   };
+
+  useEffect(() => {
+    const fetchUser = async () => {
+      await getUser.userList(post.userId).then((response) => {
+        console.log("user", response.data.user);
+        setUser(response.data.user);
+      });
+    };
+
+    fetchUser();
+  }, []);
   return (
     <div className="post">
       <div className="postWrapper">
         <div className="postAbout">
           <div className="postAboutLeft">
-            <img
-              className="postAboutLeftImg"
-              src={
-                Users.filter((user) => user.id === post.userId)[0]
-                  .profilePicture
-              }
-              alt=""
-            />
+            <Link to={`/profile/${post.userId}`}>
+              <img
+                className="postAboutLeftImg"
+                src={user.profilePicture || profileAvatar}
+                alt=""
+              />
+            </Link>
             <span className="postAboutLeftName">
-              {Users.filter((user) => user.id === post.userId)[0].username}
+              {user.firstname} {user.lastname}
             </span>
-            <span className="postAboutLeftTime">{post.date}</span>
+            <span className="postAboutLeftTime">{format(post.createdAt)}</span>
           </div>
 
           <div className="postAboutRight">
@@ -39,7 +54,7 @@ const Posts = ({ post }) => {
 
         <div className="postContent">
           <span className="postArticle">{post.desc}</span>
-          <img className="postMainImg" src={post.photo} alt="" />
+          <img className="postMainImg" src={post.image} alt="" />
         </div>
 
         <div className="postActivity">
@@ -50,7 +65,7 @@ const Posts = ({ post }) => {
             </div>
 
             <div className="postCommentsOthers">
-              <span className="postCommentNum">342</span>
+              <span className="postCommentNum">{post.comments.length}</span>
               <span className="postCommentTitle">Comments</span>
             </div>
           </div>
